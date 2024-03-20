2e950b033fc6ca0e6995e4dc5994dc1a67d23afe
Olexandr Lisovyi <microfox141120@outlook.com>
2024-03-20 03:15:48 +0200

    Updates

---
+++ assets/Description-BJx4DPMf.js
@@ -0,0 +1,1 @@
+import{j as x}from"./index-BNodiQA0.js";const s=({text:t})=>x.jsx("h6",{className:"mt-3 text-secondary text-[17px] sm:text-[18px] max-w-2xl leading-[30px]",children:t});export{s as D};
--- assets/Description-ChMDsAn5.js
+++
@@ -1,1 +0,0 @@
-const e=({text:t})=>React.createElement("h6",{className:"mt-3 text-secondary text-[17px] sm:text-[18px] max-w-2xl leading-[30px]"},t);export{e as D};
