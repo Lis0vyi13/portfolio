0c16775ddc81d3b3313e6278c3951650746889fc
Olexandr Lisovyi <microfox141120@outlook.com>
2024-03-20 03:32:58 +0200

    Updates

---
+++ assets/Description-CHDNckuN.js
@@ -0,0 +1,1 @@
+import{j as x}from"./index-Br0zZ7yM.js";const s=({text:t})=>x.jsx("h6",{className:"mt-3 text-secondary text-[17px] sm:text-[18px] max-w-2xl leading-[30px]",children:t});export{s as D};
