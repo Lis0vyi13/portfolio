5601eebdaed2cd8a4f3388f259eed6d92f5478a7
Olexandr Lisovyi <microfox141120@outlook.com>
2024-03-21 21:55:32 +0200

    Updates

--- assets/Preload-BRKBiPjl.js
+++ assets/Preload-BRKBiPjl.js
@@ -1,4 +1,4 @@
-import{r as ke,b as V0}from"./index-eUZfOc2I.js";/**
+import{r as ke,b as V0}from"./index-CtHiIU21.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
