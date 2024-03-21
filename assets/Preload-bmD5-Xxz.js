a92518ef4a4181be6d3bb49e7a0ec6a3b5bdf6cf
Olexandr Lisovyi <microfox141120@outlook.com>
2024-03-21 19:35:58 +0200

    Updates

--- assets/Preload-bmD5-Xxz.js
+++ assets/Preload-bmD5-Xxz.js
@@ -1,4 +1,4 @@
-import{r as ke,b as V0}from"./index-CLjObYuz.js";/**
+import{r as ke,b as V0}from"./index-DHIfClBa.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
