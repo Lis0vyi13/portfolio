324604678dd758ba27c48d3e33228f6b58765f87
Olexandr Lisovyi <microfox141120@outlook.com>
2024-03-21 19:21:50 +0200

    Updates

--- assets/Description-BvPxJYqS.js
+++
@@ -1,1 +0,0 @@
-import{j as x}from"./index-CPEBeinr.js";const s=({text:t})=>x.jsx("h6",{className:"mt-3 text-secondary text-[17px] sm:text-[18px] max-w-2xl leading-[30px]",children:t});export{s as D};
--- assets/Preload-BsDSO6Ay.js
+++ assets/Preload-BsDSO6Ay.js
@@ -1,4 +1,4 @@
-import{r as ke,b as V0}from"./index-CPEBeinr.js";/**
+import{r as ke,b as V0}from"./index-DWAbqnC4.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
