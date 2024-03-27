d63f1b77b43108536f82b4ceeedc242d12016ef9
Olexandr Lisovyi <microfox141120@outlook.com>
2024-03-27 19:40:12 +0200

    change margin top on smartphones

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,58 +1,58 @@
-import { useRef, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../animations/motion';
+import { useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../animations/motion";
 
-import SectionWrapper from '../hoc/SectionWrapper';
+import SectionWrapper from "../hoc/SectionWrapper";
 
-import Title from '../ui/Title';
-import Subtitle from '../ui/Subtitle';
-import Description from '../ui/Description';
+import Title from "../ui/Title";
+import Subtitle from "../ui/Subtitle";
+import Description from "../ui/Description";
 
-import { AboutText } from '../constants';
+import { AboutText } from "../constants";
 
-import lisovyi from '../assets/lisovyi.webp';
+import lisovyi from "../assets/lisovyi.webp";
 
 const About = () => {
   const about = useRef(null);
 
   useEffect(() => {
     const aboutParent = about.current.parentElement;
-    aboutParent.classList.add('mt-[-170px]');
-    aboutParent.classList.add('xs:mt-0');
+    aboutParent.classList.add("mt-[-170px]");
+    aboutParent.classList.add("xs:mt-0");
   }, []);
 
   return (
     <section
       ref={about}
-      className='relative mb-[100px] mt-[-50px] pt-[100px] xs:mt-[-10px] xs:pt-[150px] sm:mt-[20px] md:mt-[160px] md:pt-[90px] xl:mt-[-160px] xl:pt-[190px]'
+      className="relative mb-[100px] mt-[-100px] pt-[100px] xs:mt-[-10px] xs:pt-[150px] sm:mt-[20px] md:mt-[160px] md:pt-[90px] xl:mt-[-160px] xl:pt-[190px]"
     >
-      <div className='container'>
-        <div className='flex lgXl:flex-row flex-col-reverse content-center items-center gap-7 lgXl:px-[100px]'>
+      <div className="container">
+        <div className="flex lgXl:flex-row flex-col-reverse content-center items-center gap-7 lgXl:px-[100px]">
           <motion.div
-            variants={fadeIn('up', '', 0.05, 0.4)}
-            initial='hidden'
-            whileInView='show'
+            variants={fadeIn("up", "", 0.05, 0.4)}
+            initial="hidden"
+            whileInView="show"
             viewport={{ once: true, amount: 0.25 }}
           >
             <Title text={AboutText.title} />
             <Subtitle text={AboutText.subtitle} />
-            <motion.div variants={fadeIn('', '', 0.05, 0.4)}>
+            <motion.div variants={fadeIn("", "", 0.05, 0.4)}>
               <Description text={AboutText.description} />
             </motion.div>
           </motion.div>
           <div>
             <motion.div
-              variants={fadeIn('up', '', 0.05, 0.4)}
+              variants={fadeIn("up", "", 0.05, 0.4)}
               viewport={{ once: true, amount: 0.25 }}
-              className='green-pink-gradient p-[2px] text-center rounded-[30px] shadow-card w-full xs:w-[450px] h-full'
+              className="green-pink-gradient p-[2px] text-center rounded-[30px] shadow-card w-full xs:w-[450px] h-full"
             >
-              <div className='stack-card-bg rounded-[30px] p-1 min-h-[280px] flex justify-evenly items-center flex-col'>
+              <div className="stack-card-bg rounded-[30px] p-1 min-h-[280px] flex justify-evenly items-center flex-col">
                 <img
-                  className='rounded-[30px]'
-                  width='100%'
-                  height='100%'
+                  className="rounded-[30px]"
+                  width="100%"
+                  height="100%"
                   src={lisovyi}
-                  alt='lisovyi'
+                  alt="lisovyi"
                 />
               </div>
             </motion.div>
@@ -63,6 +63,6 @@ const About = () => {
   );
 };
 
-const WrappedAbout = SectionWrapper(About, 'about');
+const WrappedAbout = SectionWrapper(About, "about");
 
 export default WrappedAbout;
